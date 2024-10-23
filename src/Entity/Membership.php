723d27aef6540657d27b1a11fddd26f105f4d3cf
Wils Iglesias <wiglesias83@gmail.com>
2024-10-23 21:03:34 +0200

    Create Membership entity and new migration

---
+++ src/Entity/Membership.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MembershipRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MembershipRepository::class)]
+#[ORM\UniqueConstraint(name: 'memberships_org_user_id_uniq', columns: ['organization_id', 'user_id'])]
+final class Membership
+{
+    public function __construct(Organization $organization, User $user, Membership $role)
+    {
+        $this->organization = $organization;
+        $this->user = $user;
+        $this->role = $role;
+    }
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private Membership $role;
+
+    #[ORM\ManyToOne(targetEntity: 'User', cascade: ['persist'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private User $user;
+
+    #[ORM\ManyToOne(targetEntity: 'Organization', cascade: ['persist'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private Organization $organization;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRole(): Membership
+    {
+        return $this->role;
+    }
+
+    public function setRole(Membership $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getOrganization(): Organization
+    {
+        return $this->organization;
+    }
+
+    public function setOrganization(Organization $organization): self
+    {
+        $this->organization = $organization;
+
+        return $this;
+    }
+}
--- src/Entity/Organization.php
+++ src/Entity/Organization.php
@@ -6,7 +6,7 @@ use App\Repository\OrganizationRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: OrganizationRepository::class)]
-class Organization
+final class Organization
 {
     public function __construct(string $name, User $owner)
     {
